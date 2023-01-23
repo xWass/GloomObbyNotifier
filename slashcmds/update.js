67a43e14cb2d8db4fc559eb7ab60aef519f2c161
xWass <spectatorgaming02@gmail.com>
2023-01-22 20:02:22 -0500

    fuck off lmao

--- slashcmds/update.js
+++ slashcmds/update.js
@@ -24,7 +24,6 @@ module.exports = {
             embeds: [
                 {
                     title: "Restarting",
-                    description: stdout,
                 },
             ],
             ephemeral: true,
