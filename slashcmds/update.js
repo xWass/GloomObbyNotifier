c50ebeb3dbee29c30c1a69f5487e1faa12d53bac
xWass <spectatorgaming02@gmail.com>
2023-01-22 20:08:17 -0500

    i lied this one will work

--- slashcmds/update.js
+++ slashcmds/update.js
@@ -5,11 +5,11 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName("update")
         .setDescription("Update the bot."),
-    
+
     async execute(interaction) {
         if (interaction.user.id !== "928624781731983380") return;
         await interaction.deferReply();
-        exec("git pull", async (error, stdout) => {
+        await exec("git pull", async (error, stdout) => {
             await interaction.followUp({
                 embeds: [
                     {
@@ -19,14 +19,14 @@ module.exports = {
                 ],
                 ephemeral: false,
             });
-        });
-        await interaction.followUp({
-            embeds: [
-                {
-                    title: "Restarting",
-                },
-            ],
-            ephemeral: false,
+            await interaction.followUp({
+                embeds: [
+                    {
+                        title: "Restarting",
+                    },
+                ],
+                ephemeral: false,
+            });
         });
 
         exec("pm2 restart 2", (error, stdout) => {});
